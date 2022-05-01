928ab902084b35a6875e817c3ab6debc8f705678
rovercarlosb <carlosb20052009@gmail.com>
2022-05-01 15:59:39 -0400

    avances app para mostrar subs

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,38 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import List from './components/List';
+import { Sub } from './types';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+interface AppState {
+  subs: Sub[];
 }
 
-export default App
+const INITIAL_STATE = [
+  {
+    nick: 'Carlos',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=dapelu',
+    description: 'Hace de moredador en ocasiones',
+  },
+  {
+    nick: 'Jose',
+    subMonths: 5,
+    avatar: 'https://i.pravatar.cc/150?u=perro',
+  },
+];
+
+function App() {
+  const [subs, setSubs] = useState<AppState['subs']>(INITIAL_STATE);
+
+  useEffect(() => {}, []);
+  return (
+    <div className="App">
+      <h1>Carlos Subs</h1>
+      <List subs={subs} />
+      <Form onNewSub={setSubs} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,70 @@
+import { SetStateAction, useState } from 'react';
+import { Sub } from '../types';
+
+interface FormState {
+  inputValues: Sub;
+}
+
+interface FormProps {
+  onNewSub: React.Dispatch<React.SetStateAction<Sub[]>>;
+}
+
+const Form = ({ onNewSub }: FormProps) => {
+  const [inputValues, setInputValues] = useState<FormState['inputValues']>({
+    nick: '',
+    subMonths: 0,
+    avatar: '',
+    description: '',
+  });
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onNewSub((subs) => [...subs, inputValues]);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={inputValues.nick}
+          type="text"
+          name="nick"
+          placeholder="nick"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValues.subMonths}
+          type="number"
+          name="subMonths"
+          placeholder="subMonths"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValues.avatar}
+          type="text"
+          name="avatar"
+          placeholder="avatar"
+        />
+        <textarea
+          onChange={handleChange}
+          value={inputValues.description}
+          name="description"
+          placeholder="description"
+        />
+
+        <button type="submit">Save New Sub</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
